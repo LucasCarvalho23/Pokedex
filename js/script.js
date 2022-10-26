8f40fb11d86604a620250b7ac5b9a2e63611e80c
LucasCarvalho23 <lucasilva1990@hotmail.com>
2022-10-26 08:29:34 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
-console.log ('vasco')
+class PokedexAPI {
+
+    constructor() {
+        this.url = "https://pokeapi.co/api/v2/pokemon/"
+
+        this.inputSearch = document.querySelector ("#name-id")
+        this.buttonSearch = document.querySelector (".buttonSend-class")
+        this.form = document.querySelector ("form")
+        this.screen = document.querySelector (".screen-class")
+        this.containerResponses = document.querySelector (".container-responses")
+
+        this.form.addEventListener("submit", (e) => {
+            e.preventDefault()
+            this.searchResult()
+        })
+
+    }
+
+    async searchResult() {
+        this.url = this.url + this.inputSearch.value
+
+        this.response = await fetch (this.url)
+        this.data = await this.response.json()
+
+        this.inputName = 'Name: ' + this.data.name + '<br>' + 'Type: ' + this.data.types[0].type.name + '<br>'  + 'Abilitie: ' + this.data.abilities[0].ability.name 
+
+        console.log (this.data)
+        
+        this.containerResponses.innerHTML = this.inputName
+
+        this.valueFront = this.data.sprites.front_default
+        this.valueBack = this.data.sprites.back_default
+
+        this.form.innerHTML = "<img src = '" + this.valueFront + "'> <img src = '" + this.valueBack + "'> "
+    }
+
+}
+
+const pokedexAPI = new PokedexAPI()
